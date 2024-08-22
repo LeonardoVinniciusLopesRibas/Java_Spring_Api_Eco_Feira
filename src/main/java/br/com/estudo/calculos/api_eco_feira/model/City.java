f1aac953e3575b3621a56ec862c850547cb3c0aa
LeonardoVinniciusLopesRibas <110777424+LeonardoVinniciusLopesRibas@users.noreply.github.com>
2024-08-21 21:21:47 -0300

    Nova implementação

--- src/main/java/br/com/estudo/calculos/api_eco_feira/model/City.java
+++ src/main/java/br/com/estudo/calculos/api_eco_feira/model/City.java
@@ -1,4 +1,15 @@
 package br.com.estudo.calculos.api_eco_feira.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class City {
 }
